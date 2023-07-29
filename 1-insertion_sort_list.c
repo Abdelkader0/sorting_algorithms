db25d5600e618866130599313d799a6aad0d76d2
Abdelkader0 <ahmedbob43@gmail.com>
2023-07-29 15:37:01 -0700

    Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list
+ * @list: Pointer to the head of the list
+ * @node1: First node to be swapped
+ * @node2: Second node to be swapped
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev)
+		node1->prev->next = node2;
+	if (node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
+
+	if (!node2->prev)
+		*list = node2;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list using Insertion Sort
+ * @list: Double pointer to the head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	listint_t *current = (*list)->next;
+
+	while (current != NULL)
+	{
+		listint_t *tmp = current;
+
+		while (tmp->prev != NULL && tmp->n < tmp->prev->n)
+		{
+			swap_nodes(list, tmp->prev, tmp);
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
